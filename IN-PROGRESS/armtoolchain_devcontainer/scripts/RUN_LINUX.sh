a2b2d33c15ad943a9f3f0488e52bae65f6c03a50
Emil Jenssen <emil.jenssen@brilliant.no>
2024-06-12 17:53:22 +0200

    Added in-progress devcontainer

---
+++ IN-PROGRESS/armtoolchain_devcontainer/scripts/RUN_LINUX.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# Script to setup Docker and permission exceptions if needed
+
+# To run this script:
+# step 1, give permission: chmod +x RUN_LINUX.sh
+# step 2, run it: ./RUN_LINUX.sh
+
+# Update package list and install required packages
+echo "================================================================================"
+echo "Updating package list and installing required packages..."
+echo "================================================================================"
+echo " " 
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+
+# Add Docker's official GPG key
+echo " "
+echo "================================================================================"
+echo "Adding Docker's official GPG key..."
+echo "================================================================================"
+echo " "
+echo "Adding GPG key..."
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the Docker repository to Apt sources
+echo " "
+echo "================================================================================"
+echo "Adding Docker repository to Apt sources..."
+echo "================================================================================"
+echo " "
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install Docker packages
+echo " "
+echo "================================================================================"
+echo "Installing Docker packages..."
+echo "================================================================================"
+echo " "
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Add the current user to the docker group
+echo " "
+echo "================================================================================"
+echo "Adding the current user to the docker group..."
+echo "================================================================================"
+echo " "
+echo "running sudo usermod -aG docker $(whoami)..."
+sudo usermod -aG docker $(whoami)
+
+# Prompt for setting up a rule exception
+echo " "
+echo "================================================================================"
+read -p "Do you want to set up a rule exception for USB devices? (y/n): " setup_rule
+echo "================================================================================"
+echo " "
+
+if [[ $setup_rule == "y" || $setup_rule == "Y" || $setup_rule == "Yes" || $setup_rule == "yes" || $setup_rule == "YES" ]]; then
+    echo "Not yet implemented, work-in-progress"
+    # TODO: use lsusb to check for usb devices
+    # TODO: give options for which ones to add (as done for the windows scripts)
+#  echo "Setting up rule exception for USB devices..."
+#  sudo bash -c 'echo "SUBSYSTEMS==\"usb\", ATTRS{idVendor}==\"0483\", ATTRS{idProduct}==\"374b\", MODE:=\"0666\"" > /etc/udev/rules.d/docker-usb.rules'
+#  sudo udevadm control --reload-rules
+#  sudo udevadm trigger
+fi
+
+# Recommend restarting the computer
+echo "================================================================================"
+echo "Docker installation is complete."
+echo "It is recommended to restart your computer to apply all changes."
+echo "================================================================================"
+echo 
---
+++ IN-PROGRESS/armtoolchain_devcontainer/scripts/RUN_WIN.bat
@@ -0,0 +1,64 @@
+@echo off
+
+REM Script to Connect USB-peripheral(s) to WSL (to allow connecting to Docker containers) 
+
+echo.
+echo ================================================================================
+echo Directory Check
+echo ================================================================================
+echo.
+
+echo Checking the current directory...
+echo.
+
+REM Check if current directory is "script_files"
+REM Get the current directory path
+set "currentDir=%CD%"
+REM Extract the last component of the current directory path (the folder name)
+for %%A in ("%currentDir%") do set "currentFolder=%%~nxA"
+REM Check if the current folder name is "script_files"
+if /I "%currentFolder%" EQU "script_files" (
+    REM echo Current directory is "script_files"
+    echo Already in correct directory, "script_files".
+) else (
+    REM echo Current directory is not "script_files"
+    
+    cd script_files
+
+    REM Check if the directory change was successful
+    if %errorlevel% neq 0 (
+        echo Failed to change directory to "script_files".
+        exit /b 1
+    ) else (
+        echo Successfully changed directory to "script_files".
+    )
+    
+)
+
+
+echo.
+echo ================================================================================
+echo Running windows-start-docker.bat
+echo ================================================================================
+echo.
+
+call windows-start-docker.bat
+
+echo.
+echo ================================================================================
+echo Running windows-install-usbipd.bat
+echo ================================================================================
+echo.
+
+call windows-install-usbipd.bat
+
+echo.
+echo ================================================================================
+echo Running windows-usbipd-usb-to-wsl.bat
+echo ================================================================================
+echo.
+
+call windows-usbipd-usb-to-wsl.bat
+
+
+
